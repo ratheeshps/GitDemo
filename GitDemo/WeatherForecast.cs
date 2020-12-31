c1d052b44cb589fe2f4213f5b213e3486ecaa685
ratheesh.ps@gmail.com <ratheesh.ps@gmail.com>
2020-12-31 13:41:48 +0530

    COmmit

--- GitDemo/WeatherForecast.cs
+++ GitDemo/WeatherForecast.cs
@@ -5,7 +5,7 @@ namespace GitDemo
     public class WeatherForecast
     {
         public DateTime Date { get; set; }
-
+        //sdfd
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
